b2442e2c32790902a39e0609e440be3ab0c2cc1c
Student <student@lan.itstep.kiev.ua>
2018-07-03 19:44:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by student on 03.07.2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        int i2 = 1;
+        int i3 = 2;
+        String s = "String";
+        String s1 = "String";
+        String s2 = "other String";
+        //        Arithmetic
+        int result =  i / i2;
+
+        System.out.printIn("Result:" + result);
+        System.out.printIn(i);
+
+    }
+
+}
